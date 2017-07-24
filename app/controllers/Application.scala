c689aca660029d1604a54dcb973040313e9cdaeb
adamdye12 <adam.dye@qa.com>
2017-07-24 17:25:11 +0100

    added get methods to app

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,6 +1,8 @@
 package controllers
 
+import akka.util.ByteString
 import play.api._
+import play.api.http.HttpEntity
 import play.api.mvc._
 
 class Application extends Controller {
@@ -9,4 +11,21 @@ class Application extends Controller {
     Ok(views.html.index("Hello World"))
   }
 
+  def got = Action { implicit request: Request[AnyContent] =>
+    Ok(views.html.index("Got request [" + request + "]"))
+  }
+
+  def hello(name: String) = Action {
+    Ok("Hello" +name)
+  }
+
+  def head = Action {
+    Result(
+      header = ResponseHeader(200, Map.empty),
+      body = HttpEntity.Strict(ByteString("Hello world!"), Some("text/plain")))
+  }
+  
+  val Ok = new Status(OK)
+
+  class Status(statue: Int) extends Result(header = ResponseHeader(status), body = HttpEntity.NoEntity)
 }
