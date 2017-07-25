517d02d91397d4e05a173755a163bcb9bdb5c9e2
adamdye12 <adam.dye@qa.com>
2017-07-25 17:14:32 +0100

    removed examples and completed day one tasks

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -11,21 +11,109 @@ class Application extends Controller {
     Ok(views.html.index("Hello World"))
   }
 
-  def got = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index("Got request [" + request + "]"))
+  def redirect = Action {
+    Redirect("http://github.com")
+  }
+
+  def dynamic(input: String) = Action{
+    Ok(input)
+  }
+
+  def static = Action{
+    Ok ("static")
+  }
+
+  def default(string: String) = Action{
+    Ok ("This is the " + string)
+  }
+
+  def set (string: String) = Action{
+    Ok(string)
+  }
+
+  def optional (option: Option[String]) = Action{
+    Ok("The optinal string is " + option.getOrElse(".. hang on, there is no string here"))
+  }
+
+  val ok = Ok("Hello world!")
+  val notFound = NotFound
+  val pageNotFound = NotFound(<h1>Page not found</h1>)
+  val badRequest = BadRequest("Error happened")
+  val oops = InternalServerError("Oops")
+  val anyStatus = Status(488)("Strange response type")
+
+  def helperOk = Action{
+    ok
+  }
+
+  def helperNotFound = Action{
+    notFound
+  }
+
+  def helperPageNotFound = Action{
+    pageNotFound
+  }
+
+  def helperBadRequest = Action{
+    badRequest
+  }
+
+  def helperOops = Action{
+    oops
+  }
+
+  def helperAnyStatus = Action{
+    anyStatus
   }
 
   def hello(name: String) = Action {
-    Ok("Hello" +name)
+    Ok("Hello " + name + "!")
   }
 
-  def head = Action {
-    Result(
-      header = ResponseHeader(200, Map.empty),
-      body = HttpEntity.Strict(ByteString("Hello world!"), Some("text/plain")))
+  def helloBob = Action {
+    Redirect(routes.Application.hello("Bob"))
   }
-  
-  val Ok = new Status(OK)
+  def todo = TODO
 
-  class Status(statue: Int) extends Result(header = ResponseHeader(status), body = HttpEntity.NoEntity)
+  def sameRoute1 = Action {
+    Ok("This is the first action")
+  }
+
+  def sameRoute2 = Action {
+    Ok("This is the second action")
+  }
+
+  def createCookie = Action { implicit request: Request[AnyContent] =>
+    Ok("Cookie Created").withCookies(Cookie("theme", "blue"))
+  }
+
+  def viewCookie = Action { implicit request: Request[AnyContent] =>
+    Ok("cookies : " + request.cookies.get("theme"))
+  }
+
+  def deleteCookie = Action { implicit request: Request[AnyContent] =>
+    Ok("Cookie deleted").discardingCookies(DiscardingCookie("theme"))
+  }
+
+  def createSession = Action { implicit request: Request[AnyContent] =>
+    Ok("Session Created").withNewSession
+  }
+
+  def viewSession = Action { implicit request: Request[AnyContent] =>
+    Ok("session : " + request.session.get("connected"))
+  }
+
+
+
+  /*def viewSession = Action { request =>
+    request.session.get("connected").map { user =>
+      Ok("Hello " + user)
+    }.getOrElse {
+      Unauthorized("Oops, you are not connected")
+    }
+  }*/
+
 }
+
+
+
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -4,6 +4,6 @@ resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/release
 
 addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.5.16")
 
-addSbtPlugin("org.scalastyle" %% "scalastyle-sbt-plugin" % "0.9.0")
+//addSbtPlugin("org.scalastyle" %% "scalastyle-sbt-plugin" % "0.9.0")
 
 
